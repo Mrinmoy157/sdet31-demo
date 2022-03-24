c43c946ff22d29d36d1184188217c40359a22820
mrinm <mrinm@MRINMOY>
2022-03-24 14:58:42 +0530

    first user commit for create github pull

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/CreateGithubPullPractice.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.Demo;
+
+import org.testng.annotations.Test;
+
+public class CreateGithubPullPractice {
+	@Test
+	public void create() {
+		System.out.println("created class for pull");
+	}
+
+}
