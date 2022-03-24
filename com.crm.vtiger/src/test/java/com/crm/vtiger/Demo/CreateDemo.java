32dba7bac5888e0102dbc2ac04b6d5e669bd46f1
mrinm <mrinm@MRINMOY>
2022-03-24 12:14:35 +0530

    user 1 commit

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/CreateDemo.java
@@ -0,0 +1,13 @@
+package com.crm.vtiger.Demo;
+
+import org.testng.annotations.Test;
+
+public class CreateDemo {
+	@Test
+	public void create() {
+		System.out.println("demo class");
+		
+		
+	}
+
+}
