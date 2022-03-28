6e6a83d94e9749f4081a0727f84715557e5d9652
mrinm <mrinm@MRINMOY>
2022-03-28 13:30:58 +0530

    jenkins on pool commit

--- com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/CreateDemoTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/CreateDemoTest.java
@@ -2,7 +2,7 @@ package com.crm.vtiger.Demo;
 
 import org.testng.annotations.Test;
 
-public class CreateDemo {
+public class CreateDemoTest {
 	@Test
 	public void create() {
 		System.out.println("demo class");
---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/PracticeTest.java
@@ -0,0 +1,11 @@
+package com.crm.vtiger.Demo;
+
+import org.testng.annotations.Test;
+
+public class PracticeTest {
+	@Test
+	public void practice() {
+		System.out.println("practicing for jenkins on pool scheduling");
+	}
+
+}
