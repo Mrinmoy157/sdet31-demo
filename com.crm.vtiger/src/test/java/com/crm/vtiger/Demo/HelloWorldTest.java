b5906a83ec0c9a59e181991784224fdb58741dfb
abhis <abhis@MSI>
2022-03-29 17:41:43 +0530

    new class added

--- com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/HelloWorldTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/HelloWorldTest.java
@@ -2,6 +2,9 @@ package com.crm.vtiger.Demo;
 
 public class HelloWorldTest {
 	public void display() {
-		System.out.println("yo");
+		System.out.println("yo yo  yo  ");
+		System.out.println("yo yo  yo  ");
+		
+	
 	}
 }
