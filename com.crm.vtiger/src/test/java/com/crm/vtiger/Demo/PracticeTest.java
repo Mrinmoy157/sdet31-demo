c8d1572d7ddf3aaaeb6d8d0f45c042a07f5d30e1
abhisekc2 <102223107+abhisekc2@users.noreply.github.com>
2022-03-29 17:24:46 +0530

    see the changes

--- com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/PracticeTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Demo/PracticeTest.java
@@ -6,7 +6,7 @@ public class PracticeTest {
 	@Test
 	public void practice() {
 		System.out.println("practicing for jenkins on pool scheduling");
-		
+		System.out.println("Dont practise");
 		
 	}
 
